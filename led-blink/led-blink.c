e0ff180d0a0675f8c09bcae9dede2af8741bb5c0
J Reyes <18685018+reyesje08@users.noreply.github.com>
2021-02-18 15:54:05 -0800

    led-blink

---
+++ led-blink/led-blink.c
@@ -0,0 +1,21 @@
+// ------------ Preamble ------------
+#define    F_CPU   16000000UL
+
+#include <avr/io.h>             /*Defines pins, ports, etc*/
+#include <util/delay.h>      /* Functions to waste time*/
+
+int main(void) {
+    //------------ Inits ------------//
+    DDRB = 0b00000001;          /*
+                                    Data Direction Register B:
+                                    Writing a one to the bit enables output.
+                                */
+    // ------------ Event Loop ------------//
+    while (1) {
+        PORTB = 0b00000001;     /* Turn on first LED bit/pin in PORTB */
+        _delay_ms(1000);        /* wait */
+        PORTB = 0b00000000;     /* Turn off all B pins, including LED */
+        _delay_ms(1000);        /* wait */
+    }                           /* End event loop */
+    return (0);                 /* This line is never reached */
+}
